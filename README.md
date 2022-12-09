9c03e7f6679abe1302bb302017401064d8a32af9
Srinivas <kolipakasrinivas456@gmail.com>
2022-12-09 12:00:58 +0530

    bild from scratch one agin

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SpeedTypingGame
 React project based on useState-Hook, useEffect-Hook, useref-Hook,and custom-Hook
+
+
+
+
+1.Challenge: Using hooks, track the state of the text in the textarea on every keystroke
+  To verify it's working, you could just console.log the state on every change
+ 
+2.
--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,16 @@
-import React,{useState,useEffect} from 'react'
-import './App.css'
+import React from "react"
+
 
 function App() {
-  const [text, setText] = useState("")
-  const [timeRemaining,setTimeRemaining] = useState(5)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
-    
-  function handleChange(e) {
-      const {value} = e.target
-      setText(value)
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  }
-
-  useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-            setTimeRemaining(time => time - 1)
-        }, 1000)
-    } else if(timeRemaining === 0) {
-        setIsTimeRunning(false)
-    }
-}, [timeRemaining, isTimeRunning])
-
-  
-
-  return (
-    <div className="App">
-       <h1>How fast do you type?</h1>
-            <textarea 
-            onChange={handleChange}
-                value={text}
-            />
-            <h4>Time reminaing:{timeRemaining}</h4>
-            <button onClick={() => setIsTimeRunning(true)}>Start</button>
-            <h1>Word count: {text}</h1>
-    </div>
-        
-  )
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea />
+            <h4>Time reminaing: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
+        </div>
+    )
 }
 
 export default App
